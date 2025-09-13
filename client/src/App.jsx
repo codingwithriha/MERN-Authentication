2e500243572e9df67747382f78055f17789bdb7a
Codingwithriha <rihashehzadi2003@gmail.com>
2025-09-13 11:01:54 +0500

    mern-auth

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import { EmailVerify } from './pages/EmailVerify'
+import { ResetPassword } from './pages/ResetPassword.jsx'
+import { Home } from './pages/home'
+import { ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const App = () => {
+  return (
+    <div>
+      <ToastContainer />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/email-verify' element={<EmailVerify />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
